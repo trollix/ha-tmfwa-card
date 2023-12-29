3003624d8118301472c8db6c86e4cc72bfd97b7a
trollix <dr.chandra@laposte.net>
2023-12-29 14:08:35 +0100

    release 0.0.1

---
+++ README.md
@@ -0,0 +1,68 @@
+# Pollens France (from RNSA)
+
+![Tix pollens FR](https://img.shields.io/github/v/release/trollix/ha-tpollensfr-card)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg?style=flat)](https://github.com/custom-components/hacs)
+
+Cette carte "Lovelace custom card" affiche les données fournies par le Réseau National de Surveillance Aérobiologique (R.N.S.A.) [RNSA France](https://www.pollens.fr/) .
+Cette intégratuion est compatible HACS.
+
+## Preview
+
+![Pollen France Card](https://github.com/trollix/ha-tpollensfr-card/blob/main/img01.png?raw=true "Pollen France Card")
+
+![Pollen France Card ](https://github.com/trollix/ha-tpollensfr-card/blob/main/img02.png?raw=true "Pollen France Card")
+
+### Installation
+
+The easiest way to install it is through [HACS (Home Assistant Community Store)](https://github.com/hacs/frontend),
+search for *Pollen France* in the Frontend section and select "Pollens France (from RNSA)".
+If you are not using HACS, you may download tpollensfr-card.js and put it into
+homeassistant_config_dir/www/community/tix-pollens-france/ directory.
+
+### Configuration dans Lovelace UI
+
+Please add the card to the resources in configuration.yaml:
+
+``` resources:
+  - {type: module, url: '/hacsfiles/ha-tpollensfr-card/tpollensfr-card.js'}
+```
+
+### Options
+
+#### Options de la carte
+
+| Name             | Type         | Required     | Default         | Description                                        |
+| ---------------- | ------------ | ------------ | --------------- | -------------------------------------------------- |
+| type             | string       | **required** |                 | `custom:tpollensfr-card`                            |
+| title            | string       | optional     | `Pollens`       | title                                              |
+| sensor_name      | string       | optional     | `pollens2`      | HA sensor name                                     |
+| minimum_level    | integer      | optional     | `1`             | Affiche les pollens de niveau sup. à minimum_level |
+
+Les valeurs sont celles fournies  par le RNSA (source: [RNSA](https://www.pollens.fr/le-reseau/les-pollens/)):
+
+| Description | Concentration       | Couleur    |
+| ----------- | ------------------- | ---------- |
+| nul         | 0                   | gris       |
+| faible      | 1                   | vert       |
+| moyen       | 2                   | orange     |
+| élevé       | 3                   | rouge      |
+
+Voici un exemple de configuration dans le paramétrage de la carte:
+
+```yaml
+    type: custom:tix-pollens-fr-card
+    above_level: 1  
+    title: Pollens
+```
+
+![Pollen France Card ](https://github.com/trollix/ha-tpollensfr-card/blob/main/img03.png?raw=true "Pollen France Card")
+
+Niveaux du risque allergique de l'ensemble des pollens répertoriés: , mettre en configuration minimum_level=0:
+
+![Pollen France Card ](https://github.com/trollix/ha-tpollensfr-card/blob/main/img04.png?raw=true "Pollen France Card")
+
+## Merci
+
+Merci à tous les utilisateurs de leurs remontées de problèmes et de bugs divers.
---
+++ tmfwa-card.js
@@ -0,0 +1,156 @@
+const LitElement = Object.getPrototypeOf(customElements.get("ha-panel-lovelace"));
+const html = LitElement.prototype.html;
+const css = LitElement.prototype.css;
+
+const CARD_VERSION = '0.0.1';
+
+console.info(
+  `%c  TMFWA-CARD  %c  Version ${CARD_VERSION}  `,
+  'color: white; font-weight: bold; background: crimson',
+  'color: #000; font-weight: bold; background: #ddd',
+);
+
+function hasConfigOrEntityChanged(element, changedProps) {
+  if (changedProps.has("config")) {
+    return true;
+  }
+  
+  const oldHass = changedProps.get("hass");
+  if (oldHass) {
+    return (
+      oldHass.states[element.config.entity] !==
+        element.hass.states[element.config.entity]
+    );
+  }
+  
+  return true;
+}
+  
+class TMFWACard extends LitElement {
+  
+  static get properties() {
+    return {
+      config: {},
+      hass: {},
+    };
+  }
+  
+  _getPollens(hass, sensor_name, above_level) {
+    var res = [];
+
+    if (typeof hass.states[`sensor.${sensor_name}`] != "undefined") {
+      const data1 = hass.states[`sensor.${sensor_name}`].attributes['risks'];
+      //console.log(data1);
+      Object.keys(data1 || {}).forEach(function (key) {
+        if ( parseInt(data1[key].level, 10) >= above_level ) {
+          res.push({
+            name: data1[key].pollenName,
+            concentration: "level" + data1[key].level,
+          });
+        }
+      });
+    }
+    return res;
+  }
+  
+  setConfig(config) {
+    const defaultConfig = {
+      'no_pollens_label': 'No pollens',
+      'sensor_name': 'pollens2',
+      'above_level': 1,
+      'title': 'Pollens',
+      'icon': 'mdi:square'
+    }
+  
+    this.config = {
+      ...defaultConfig,
+      ...config
+    };
+  }
+  
+  render() {
+    if (!this.config || !this.hass) {
+      return html``;
+    }
+    //console.log('pollens fr')
+    const pollens = this._getPollens(this.hass, this.config.sensor_name, this.config.above_level);
+    return html`
+      <ha-card header="${this.config.title}">
+          <div id="tixpollenfr">
+          ${pollens.length > 0
+            ? html`<div class="ok-pollen">${pollens.map(pollen => this.renderPollen(pollen))}</div>`
+            : html`<div class="no-pollen">${this.config.no_pollens_label}</div>`
+          }
+          </div>
+      </ha-card>
+    `;
+  }
+  
+  renderPollen(pollen) {
+    return html
+    `
+      <div class="in-pollen"><ha-icon icon="${this.config.icon}" class="${pollen.concentration} levelicon"></ha-icon>
+      ${pollen.name}</div>
+    `;
+  }
+  
+  getCardSize() {
+    return this.config.entities.length + 1;
+  }
+  
+  static get styles() {
+    return css`
+    #tixpollenfr {
+      margin-top: 0.4em;
+      padding-bottom: 0.8em;
+      display: flex;
+    }
+    .ok-pollen {
+      margin-left: 2em;
+      margin-right: 2em;
+      margin: auto;
+      display: float;
+      width: auto;
+    }
+    .in-pollen {
+       margin: 0px 0px 0px 15px;
+       padding: 1px 0px 0px 0px;
+       float: left;
+       position: relative;
+       width: 125px;
+    }
+    .level0 {
+      color: grey;
+    }
+    .level1 {
+      color: green;
+    }
+    .level2 {
+      color: orange;
+    }
+    .level3 {
+      color: red;
+    }
+    .level4 {
+      color: red;
+    }
+    .levelicon {
+      --mdc-icon-size: 2em;
+    }
+    .no-pollens {
+      margin-left: 1.4em;
+    }
+    `;
+  }
+}
+  
+customElements.define('tmfwa-card', TMFWACard);
+  
+// Puts card into the UI card picker dialog
+(window).customCards = (window).customCards || [];
+(window).customCards.push({
+  type: 'tmfwa-card',
+  name: 'Meteo France Weather Alert Card',
+  preview: true,
+  description: 'This Lovelace custom card displays Meteo France Weather Alert information provided by French Weather Service.',
+});
